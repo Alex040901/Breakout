c9c5ad7a3c1d678844128844fa1b1a15c60e56cd
AlejandroCD <alejandrocastroduarte87@gmail.com>
2026-09-08 16:57:28 -0600

    feat: colision con paddle y con bordes

--- main.py
+++ main.py
@@ -12,17 +12,27 @@ ball = (x, y)
 #Radio de la bola
 radio = 10
 
-#Posición del paddle
-dx1, dx2 = 500, 650
-width2, height2 = 100, 20
+ball_x, ball_y = 100, 100
 
-# Definiendo velocidad del objeto
-speed = 5
+#Posición del paddle
+paddle_x, paddle_y = 500, 650
+paddle_width, paddle_height = 100, 20
+
+paddle = pygame.Rect(paddle_x, paddle_y, paddle_width, paddle_height)
+
+#Posición de la bola
+ball_position = pygame.Rect(ball_x, ball_y, 20, 20)
+ball_position.center = (ball_x, ball_y)
+
+total_lives = 3
+print("Total de vidas", total_lives)
 
 # Definiendo color de canvas
 black = 0, 0, 0
 white = 255, 255, 255
 blue = 0, 0, 255
+
+ball_speed = [5, 5]
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("BREAKOUT")
@@ -34,25 +44,49 @@ while running:
     clock.tick(60)
 
     keys = pygame.key.get_pressed()
+    paddle_speed = 5
+    ball_position = ball_position.move(ball_speed)
 
     if keys[pygame.K_LEFT]:
-        dx1 -= speed
-        dx2 = 650
+        paddle.x -= paddle_speed
+    
     if keys[pygame.K_RIGHT]:
-        dx1 += speed
-        dx2 = 650
+        paddle.x += paddle_speed
+
+    if paddle.left < 0:
+        paddle.left = 0
+    if paddle.right > width:
+        paddle.right = width
+
+    if ball_position.left < 0 or ball_position.right > width:
+        ball_speed[0] = -ball_speed[0]
+    if ball_position.top < 0 or ball_position.bottom > height:
+        ball_speed[1] = -ball_speed[1]
+
+    if ball_position.bottom > height:
+        print("-1UP")
+        total_lives -= 1
+        print("Vidas ", total_lives)
+
+
+    if paddle.colliderect(ball_position) and ball_speed[1] > 0:
+        print("Collision detected")
+        ball_speed[1] = -abs(ball_speed[1])
+
+        ball_position.bottom = paddle.top
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                running = False  
-
+                running = False          
     
     screen.fill(black)
-    pygame.draw.circle(screen, white, ball, radio)
-    pygame.draw.rect(screen, blue, (dx1, dx2, width2, height2))
+    pygame.draw.circle(screen, white, ball_position.center, radio)
+    pygame.draw.rect(screen, (255, 0, 0), ball_position, 2)
+
+    pygame.draw.rect(screen, blue, paddle)
     pygame.display.flip()
 pygame.quit()
 sys.exit()
